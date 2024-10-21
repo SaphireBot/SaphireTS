f20676c139181c5b9cd5401323e5a10409ebb4cc
Rodrig Couto <rodrigo.couto.santos@hotmail.com>
2024-10-21 18:40:27 -0300

    ,

--- src/services/api/ws/funtions/staffData.ts
+++ src/services/api/ws/funtions/staffData.ts
@@ -8,19 +8,22 @@ export default async function staffData(socket: Socket) {
         devs: ["451619591320371213", "395669252121821227", "920496244281970759", "435601052755296256", "648389538703736833"], // Rody, Gorniaky, Space, Lucaix, André
         admins: ["451619591320371213", "351903530161799178"], // Rody, Makol
         boards: ["395669252121821227", "648389538703736833"], // Gorniaky, André
-        staff: ["854494598533742622", "611006830411251714", "781137239194468403", "830226550116057149", "327496267007787008", "144943581965189120", "435444989695229952", "674295513742442573"] // Akemy, Alli, Dspofu, Pepy, San, Serginho, Yafyr, Liege
+        staff: ["611006830411251714", "781137239194468403", "830226550116057149", "327496267007787008", "144943581965189120", "435444989695229952", "674295513742442573"], // Ally, Dspofu, Pepy, San, Serginho, Yafyr, Liege
     };
 
     const ids = Array.from(
         new Set(
-            Object.values(tagsAndIds).flat()
-        )
+            Object.values(tagsAndIds).flat(),
+        ),
     );
 
     const staffers = await Promise.all(
-        ids.map(id => client.users.fetch(id))
+        ids.map(id => client.users.fetch(id)),
     )
-        .then(users => users.filter(Boolean));
+        .then(users => users
+            .filter(Boolean)
+            .filter(user => !user.username.includes("deleted_user")),
+        );
 
     for (const staff of staffers as any[]) {
 
