c51baf1f4ebff55f76e2d8ee34ebb40700382fa0
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2025-09-25 15:30:25 -0300

    Update Message.ts
    parseMemberMentions
    from djs-protofy to saphire

--- src/@prototypes/Message.ts
+++ src/@prototypes/Message.ts
@@ -1,8 +1,10 @@
-import { Message, LocaleString } from "discord.js";
+import { Message, LocaleString, GuildMember, Collection, GatewayIntentBits } from "discord.js";
 import Database from "../database";
 import { locales } from "./User";
 import { Config } from "../util/constants";
 import client from "../saphire";
+
+const ManyDiscordSnowflakesPattern = /\d{17,}/g;
 
 Message.prototype.locale = async function (): Promise<any> {
     const locale = locales.get(this.author?.id);
@@ -20,3 +22,40 @@ Message.prototype.locale = async function (): Promise<any> {
 
     return data || client.defaultLocale;
 };
+
+Message.prototype.parseMemberMentions = async function (): Promise<Collection<string, GuildMember>> {
+ if (!this.guild) return this.mentions.members ?? new Collection();
+
+    if (this.client.options.intents.has(GatewayIntentBits.GuildMembers)) {
+      // @ts-expect-error ts(2339)
+      if (!this.guild._membersHasAlreadyBeenFetched) {
+        await this.guild.members.fetch({ time: 10000 }).catch(() => null);
+        // @ts-expect-error ts(2339)
+        this.guild._membersHasAlreadyBeenFetched = true;
+      }
+    } else {
+      const users = Array.from(new Set(this.content.match(ManyDiscordSnowflakesPattern)));
+
+      if (users.length) {
+        for (let i = 0; i < users.length;) {
+          const user = users.slice(i, i += 100);
+          // eslint-disable-next-line no-await-in-loop
+          await this.guild.members.fetch({ user, time: 1000 }).catch(() => null);
+        }
+      }
+    }
+
+    const queries = new Set(this.content.trim().split(/\s+/g));
+
+    for (const query of queries) {
+      if (this.mentions.members!.has(query)) continue;
+
+      const member = this.guild.members.searchBy(query);
+
+      if (!member || this.mentions.members!.has(member.id)) continue;
+
+      this.mentions.members!.set(member.id, member);
+    }
+
+    return this.mentions.members!;
+}
