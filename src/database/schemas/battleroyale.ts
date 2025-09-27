8e8f5bf734e2689d82f6131ee0c1d73bdfd4481e
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2025-09-27 16:52:17 -0300

    Update battleroyale.ts

--- src/database/schemas/battleroyale.ts
+++ src/database/schemas/battleroyale.ts
@@ -9,4 +9,13 @@ export const BattleroyaleSchema = new Schema({
     wins: Number
 });
 
+export const BattleroyalePhraseSchema = new Schema({
+    user: String,
+    players: Number, // 1 | 2 | 3 players
+    kill: Boolean,
+    phrase: String,
+    approved: Boolean
+});
+
 export type BattleroyaleSchemaSchemaType = InferSchemaType<typeof BattleroyaleSchema> & { _id: Types.ObjectId };
+export type BattleroyalePhraseSchemaType = InferSchemaType<typeof BattleroyalePhraseSchema> & { _id: Types.ObjectId };
